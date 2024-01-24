c4fb302318c3f637fcdfd548695881e5c5e9efd9
Sajib Khan <sajibkhan.mern@gmail.com>
2024-01-25 00:14:30 +0600

    firebase setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
+import { getDatabase, push, ref, set } from "firebase/database";
 
 const App = () => {
+  const db = getDatabase();
+  let [input,setInput]=useState("")
+  let [all,setAll]=useState([])
+  let handleClick=()=>{
+    set(push(ref(db, 'users/' )), {
+      name:input
+    }).then(()=>{
+      setInput('')
+      
+    });
+    
+  }
+  
   return (
-    <div className='bg-red-500 '>App</div>
+    <div className=''>
+      <input value={input} type="text" onChange={(e)=>setInput(e.target.value)}  className=' border-solid border'/>
+      <button onClick={handleClick} className='bg-red-200 py-2 px-10 rounded'>Add</button>
+    </div>
   )
 }
 
---
+++ src/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC21gxtWdkybZ-t1JJHyKHb-E3m33vw9r4",
+  authDomain: "gallery-81ef9.firebaseapp.com",
+  projectId: "gallery-81ef9",
+  storageBucket: "gallery-81ef9.appspot.com",
+  messagingSenderId: "628305092000",
+  appId: "1:628305092000:web:60f969a162a7fb999018c5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default firebaseConfig
