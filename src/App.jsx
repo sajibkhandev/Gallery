ab816847ac5e1f34f63a638b321d510186265cfd
Sajib Khan <sajibkhan.mern@gmail.com>
2024-01-29 23:58:07 +0600

    text

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
      <h2>this is make for gallery exam</h2>
      <h3>it created for exam</h3>
      <h4>There are some feature add</h4>
+     <p>tailwind setput</p>
     </div>
   )
 }
