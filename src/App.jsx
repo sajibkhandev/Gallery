8702cbf4ec07cd2ec6d96bb69cd602b3a109da32
Sajib Khan <sajibkhan.mern@gmail.com>
2024-01-29 23:58:57 +0600

    text delete

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ const App = () => {
   return (
     <div>
      <h2>this is make for gallery exam</h2>
-     <h3>it created for exam</h3>
-     <h4>There are some feature add</h4>
-     <p>tailwind setput</p>
+     
     </div>
   )
 }
